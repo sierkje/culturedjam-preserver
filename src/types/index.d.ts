21f3de2c132d91ba9db432383a5ea7c8dfaddedc
Sierk Beij <sierk@riseup.net>
2022-07-04 14:49:31 -0700

    Add ambient type for babel-plugin-dev-expression.

---
+++ src/types/index.d.ts
@@ -0,0 +1,11 @@
+/**
+ * This file holds ambient type declarations.
+ */
+
+/**
+ * Shorthand for: `process.env.NODE_ENV === "development"`
+ *
+ * @see https://github.com/4Catalyzer/babel-plugin-dev-expression#__dev__
+ */
+// eslint-disable-next-line no-underscore-dangle
+declare const __DEV__: boolean
