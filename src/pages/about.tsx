a6c076c93a12517d3df8090242a70023e3601d42
Sierk Beij <sierk@riseup.net>
2022-07-05 01:07:25 -0700

    Add the missing About page.

---
+++ src/pages/about.tsx
@@ -0,0 +1,10 @@
+function AboutPage() {
+  return (
+    <main>
+      <h1>About Cultured Jam</h1>
+      <p>This website needs some content and the code needs some tests.</p>
+    </main>
+  )
+}
+
+export default AboutPage
