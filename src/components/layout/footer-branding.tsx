32343f1d52730706373a3ee0348ef58feff8ba39
Sierk Beij <sierk@riseup.net>
2022-07-05 08:18:22 -0700

    Update footer branding styling.

--- src/components/layout/footer-branding.tsx
+++ src/components/layout/footer-branding.tsx
@@ -1,5 +1,8 @@
 import { css } from "@emotion/react"
 
+import cssVar from "../../helpers/css-var"
+import { COLOR_SCHEME_CLASS_NAME } from "../../hooks/use-color-scheme"
+
 import JarOfCulturedJam from "@/components/jar-of-cultured-jam"
 import useSiteSlogan from "@/hooks/use-site-slogan"
 import themeConstants from "@/theme-constants"
@@ -11,7 +14,13 @@ const wrapperCss = css`
   column-gap: 0.25rem;
   grid-template-columns: 1fr auto;
   grid-template-rows: 1fr;
+  opacity: 0.95;
   width: 100%;
+
+  .${COLOR_SCHEME_CLASS_NAME} & {
+    opacity: 0.5;
+    filter: invert();
+  }
 `
 
 const logoCss = css`
@@ -20,18 +29,28 @@ const logoCss = css`
   height: 100%;
   justify-items: left;
   margin: 0 -1rem;
+  fill-opacity: 0.6;
+  opacity: 0.5;
+  stroke-opacity: 0.8;
 `
 
 const sloganCss = css`
   align-items: center;
   display: flex;
-  font-size: 1.5rem;
+  font-family: ${cssVar(`--fancy-font-family`)};
+  font-size: 1.25rem;
+  font-weight: ${cssVar(`--fancy-font-weight`)};
   line-height: 1.25;
   height: 100%;
   text-align: right;
+  text-transform: uppercase;
+
+  @media (min-width: 450px) and (max-width: 649.99px) {
+    font-size: 1.45rem;
+  }
 
   @media (min-width: 650px) {
-    font-size: 2rem;
+    font-size: 1.65rem;
   }
 `
 
@@ -39,7 +58,7 @@ function LayoutFooterBranding() {
   const slogan = useSiteSlogan()
 
   return (
-    <p css={wrapperCss}>
+    <p css={wrapperCss} aria-hidden>
       <span css={logoCss}>
         <JarOfCulturedJam isEmpty size={BASE_FONT_SIZE_PX * 8} />
       </span>
